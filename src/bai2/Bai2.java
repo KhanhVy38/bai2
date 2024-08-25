8f30a9d78026e205be323dec7c672ac5553da458
KhanhVy38 <khanhvyken50@gmail.com>
2024-08-25 10:37:29 +0700

    khanhvy

---
+++ src/bai2/Bai2.java
@@ -0,0 +1,14 @@
+package bai2;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter data: ");
+        //boolean check1=scanner.hasNextInt();
+        boolean check2= scanner.hasNextDouble();
+        System.out.println("CO Token khong?"+check2);
+    }
+    
+}
